c00cf7d270d90cdf5cbf4f99a7a317fb96ca9230
Rey Villamar1 <reyvillamar@gmail.com>
2017-04-26 23:32:35 +0800

    fixing hs

--- app/Http/Controllers/GmpController.php
+++ app/Http/Controllers/GmpController.php
@@ -31,43 +31,42 @@ class GmpController extends Controller
     public function hswebhook(){
             //$testJson = '[{"objectId":8501,"propertyName":"hs_lead_status","propertyValue":"DEAL","changeSource":"API","eventId":4083751833,"subscriptionId":3167,"portalId":3088964,"appId":39543,"occurredAt":1493051724096,"subscriptionType":"contact.propertyChange","attemptNumber":0}]';
 
-            $file = fopen("test.txt","a+");
+
             $json = file_get_contents('php://input');
-            echo fwrite($file,$json);
-            fclose($file);
+            $datahs = json_decode($json,true);
+            $objectId = $datahs[0]["objectId"];
+            $propertyName = $datahs[0]["propertyName"];
+            $propertyValue = $datahs[0]["propertyValue"];
 
-//            $datahs = json_decode($json,true);
-//            $objectId = $datahs[0]["objectId"];
-//            $propertyName = $datahs[0]["propertyName"];
-//            $propertyValue = $datahs[0]["propertyValue"];
-//
-//
-//            $contact = Contact::where("vid",$objectId)->first();
-//            if($propertyName == "hs_lead_status"){
-//                $arrGroupID = array(['579048','NEW'],
-//                    ['579466','RESPONSE'],
-//                    ['579531','WARM'],
-//                    ['579657','DEAL']);
-//
-//                $groupID = "";
-//                foreach($arrGroupID as $data){
-//                  if($data[1] == $propertyValue){
-//                      $groupID = $data[0];
-//                  }
-//                }
-//                $contact->GroupId = $groupID;
-//
-//                $contact->save();
-//
-//                $this->update_gmp($groupID,$contact->LeadId,'groupId');
-//                var_dump($groupID);
-//              //  echo "propertyName".$propertyName;
-//                //Find in the database
-//            }else{
-//                //Do to other attributes here like Firstname, Lastname etc
-//            }
-//            //var_dump($datahs);
 
+            $contact = Contact::where("vid",$objectId)->first();
+            if($propertyName == "hs_lead_status"){
+                $arrGroupID = array(['579048','NEW'],
+                    ['579466','RESPONSE'],
+                    ['579531','WARM'],
+                    ['579657','DEAL']);
+
+                $groupID = "";
+                foreach($arrGroupID as $data){
+                  if($data[1] == $propertyValue){
+                      $groupID = $data[0];
+                  }
+                }
+                $contact->GroupId = $groupID;
+
+                $contact->save();
+
+                $this->update_gmp($groupID,$contact->LeadId,'groupId');
+
+              //  echo "propertyName".$propertyName;
+                //Find in the database
+            }else{
+                //Do to other attributes here like Firstname, Lastname etc
+            }
+            //var_dump($datahs);
+        $file = fopen("test.txt","a+");
+        echo fwrite($file,$json);
+        fclose($file);
         echo "Success";
     }
 
